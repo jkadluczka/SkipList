0b1da53ccf5904fbaf9c04073563994977b43cab
Jacek Kadluczka <jackad@o2.pl>
2018-02-14 15:18:35 +0100

    Fixed List constructor, commentary added v2

--- src/list/list.js
+++ src/list/list.js
@@ -11,15 +11,14 @@ export class List {
   constructor (comp) {
     this.root = null;                   //Constructor is using one node and sets it as root of list
     this.comparator = comp;
+
     if (this.comparator === undefined) {
       this.comparator = (node1, node2) => {
         if (node1 > node2) {
           return 1;
-        }
-        else if (node1 < node2) {
+        } else if (node1 < node2) {
           return -1;
-        }
-        else {
+        } else {
           return 0;
         }
       };
@@ -33,13 +32,16 @@ export class List {
    * @returns {Node}
    */
   find (value) {
+    let node = this.root;                   //Creating new Node for operations on list
+
     if (!(value instanceof Node)) {         //Checking if "value" is instance of Node
       value = new Node(value);            //If not, creating new Node named "value" with .value equals to function argument
     }
-    let node = this.root;                   //Creating new Node for operations on list
+
     while (value.value !== node.value) {    //Looking fot matching values
       node = node.next;                   //If values are not matching its moving on to next node
     }
+
     return node;
   }
 
@@ -53,20 +55,19 @@ export class List {
     if (!(value instanceof Node)) {     //Checking if "value" is instance of Node
       value = new Node(value);        //If not, creating new Node named "value" with .value equals to function argument
     }
+
     if (this.root === null) {
       this.root = value;
-      node = value;
-    }
-    else {
+    } else {
       if (this.comparator(value, node) === -1) { //Checking if inserted node would not become new root
         value.next = node;                     //If so, "value" node becomes root
         this.root = value;
-      }
-      else {
+      } else {
         while (node.next !== null &&
         this.comparator(node.next.value, value.value) === -1) { //Navigating to the node which woulde be just right before insertion.
           node = node.next;                                                    //In the same time checking if we didnt get to the end of the list
         }
+
         value.next = node.next;          //Changing node pointers so insertion would be complete
         node.next = value;
       }
@@ -84,13 +85,14 @@ export class List {
     if (!(value instanceof Node)) { //Checking if "value" is instance of Node
       value = this.find(value);   //If not, creating new Node named "value" with .value equals to function argumen
     }
+
     if (value === this.root) {      //Checking if node that we want to delete isn't root
       this.root = this.root.next; //If so, reassigning root
-    }
-    else {
+    } else {
       while (this.comparator(node.next.value, value.value) === -1) {     //Navigating to node right before one to delete
         node = node.next;
       }
+
       node.next = value.next;                     //Changing node pointer so node deleting is complete
     }
   }
@@ -107,6 +109,7 @@ export class List {
       array.push(node.value);
       node = node.next;
     }
+
     return array;
   }
 
