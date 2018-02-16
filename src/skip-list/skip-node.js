9746c272244f4837b1718656a99cd7e97df245b0
Jacek Kadluczka <jackad@o2.pl>
2018-02-16 09:11:41 +0100

    Name changed (package.json)

--- src/skip-list/skip-node.js
+++ src/skip-list/skip-node.js
@@ -2,15 +2,15 @@ const DEFAULT_HEIGHT = 10;
 
 export class SkipNode {
   constructor (value, height = DEFAULT_HEIGHT) {
-    this.value = value;         //value of node
-    this.stack = {};
+    this.value = value;                     //value of node
+    this.stack = {};                        //Poiters declared as objects
 
-    for (let h = 0; h < height; h++) {
+    for (let h = 0; h < height; h++) {    //setting all pointers at 'null'
       this.stack[h + 1] = null;
     }
   }
 
   get height () {
-    return Object.keys(this.stack).length;
+    return Object.keys(this.stack).length;    //height getter
   }
 }
