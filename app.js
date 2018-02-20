ebd2cc0ed2ee890fc39007de32dceae33832e2c6
Jacek Kadluczka <jackad@o2.pl>
2018-02-20 11:11:46 +0100

    Successor and predecessor working

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 import {Bst,Node} from './src/bst';
 
-let noud = new Node(6)
+let noud = new Node(5);
 let bst = new Bst();
 
+bst.add(50);
+bst.add(30);
+bst.add(70);
 bst.add(10);
-bst.add(5);
-bst.add(15);
-bst.add(12);
-bst.add(17);
-bst.add(13);
-bst.add(1);
+bst.add(40);
+bst.add(60);
+bst.add(80);
 bst.add(noud);
+bst.add(15);
+bst.add(35);
+bst.add(45);
+bst.add(55);
+bst.add(65);
+bst.add(75);
+bst.add(90);
 
-console.log(bst.find(noud));
+console.log(bst._findSuccessor(noud));
+console.log(bst._findPredecessor(60));
--- src/bst/bst.js
+++ src/bst/bst.js
@@ -1,8 +1,8 @@
 import { Node } from './node';
 
-const COMPARATOR = (a,b) => a>b;
+const COMPARATOR = (a, b) => a > b;
 const DEFAULT_ARGS = {
-  comparator : COMPARATOR
+  comparator: COMPARATOR,
 };
 
 export class Bst {
@@ -22,7 +22,7 @@ export class Bst {
     } else {
 
       while (iterator !== null) {
-        if (this.compare(node.value,iterator.value)) {
+        if (this.compare(node.value, iterator.value)) {
           if (iterator.rightChild !== null) {
             iterator = iterator.rightChild;
           } else {
@@ -30,7 +30,7 @@ export class Bst {
             node.parent = iterator;
             break;
           }
-        } else if (this.compare(iterator.value,node.value)) {
+        } else if (this.compare(iterator.value, node.value)) {
           if (iterator.leftChild !== null) {
             iterator = iterator.leftChild;
           } else {
@@ -39,7 +39,6 @@ export class Bst {
             break;
           }
         }
-
       }
     }
   }
@@ -47,49 +46,95 @@ export class Bst {
   find (node) {
     let iterator = this.root;
 
+    if (!(node instanceof Node)) {
+      node = new Node(node);
+    }
 
-    while (node === null) {
-      if (iterator.value === node.value) {
-        node = iterator;
-        break;
-      } else if (node.value > iterator.value) {
+    while (node !== iterator) {
+      if (this.compare(node.value, iterator.value)) {
         if (iterator.rightChild !== null) {
           iterator = iterator.rightChild;
         } else {
           node = iterator;
           break;
         }
-      } else if (node.value < iterator.value) {
+      } else if (this.compare(iterator.value, node.value)) {
         if (iterator.leftChild !== null) {
           iterator = iterator.leftChild;
         } else {
           node = iterator;
           break;
         }
+      } else {
+        node = iterator;
       }
     }
-
     return node;
   }
 
-  _findSuccessor(node){
-    let iterator = this.root;
+  _findSuccessor (node) {
+    let iterator = this.find(node);
+    let leftChild;
 
-    if (!(node instanceof Node)) {
-      node = new Node(node);
+    if (iterator.rightChild !== null) {
+      iterator = iterator.rightChild;
+      while (iterator.leftChild !== null) {
+        iterator = iterator.leftChild;
+      }
+      return iterator;
+    } else {
+      while (iterator.leftChild !== leftChild) {
+        leftChild = iterator;
+        iterator = iterator.parent;
+        if(iterator === null) {
+          break;
+        }
+      }
+      if(iterator === null)
+      {
+        return "No predecessor found"
+      }else {
+        return iterator;
+      }
     }
+  }
+
+  _findPredecessor (node) {
+    let iterator = this.find(node);
+    let rightChild;
+
+    if (iterator.leftChild !== null) {
+      iterator = iterator.leftChild;
+      while (iterator.rightChild !== null) {
+        iterator = iterator.rightChild;
+      }
+      return iterator;
+    } else {
+      while (iterator.rightChild !== rightChild) {
+        rightChild = iterator;
+        iterator = iterator.parent;
+        if(iterator === null) {
+          break;
+        }
+      }
+      if(iterator === null)
+      {
+        return "No predecessor found"
+      }else {
+        return iterator;
+      }
+    }
+  }
+
+  toArray () {
 
   }
-  _findPredecessor(node){
+
+  toString () {
 
   }
-  toArray(){
 
-  }
-  toString(){
-
-  }
-  balance(){
+  balance () {
 
   }
 }
